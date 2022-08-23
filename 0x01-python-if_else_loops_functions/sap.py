1b7b15f327c615ac4d8efd0084c39ef821a53e30
Jonathan Okah <jonaben7@gmail.com>
2022-08-23 15:48:01 +0100

    finding tthe last digit number

---
+++ 0x01-python-if_else_loops_functions/sap.py
@@ -0,0 +1,32 @@
+# Python3 program to find first and 
+# last digits of a number
+  
+# Find the first digit
+def firstDigit(n) :
+  
+    # Remove last digit from number
+    # till only one digit is left
+    while n >= 10: 
+        n = n / 10;
+      
+    # return the first digit
+    return int(n)
+  
+# Find the last digit
+def lastDigit(n) :
+  
+    # return the last digit
+    return (n % 10)
+  
+# Driver Code
+n = -98562;
+print(firstDigit(n), end = " ") 
+print(lastDigit(n))
+  
+# This code
+
+x = firstDigit(-12345)
+y= lastDigit(-12345)
+
+print(f"12345:{x}, {y}")
+ 
