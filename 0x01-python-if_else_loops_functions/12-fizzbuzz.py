54e8dc526eb9813d0c96cbd184e687f31e22dc6d
Jonathan Okah <jonaben7@gmail.com>
2022-08-23 22:14:46 +0100

    functions in python

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 def fizzbuzz():
     for i in range(1, 100):
         if i % 3 == 0:
-            print("fizz", end=' ')
+            print("Fizz", end=' ')
         elif i % 5 == 0:
             print("Buzz", end=' ')
         elif (i % 3 == 0) & (i % 5 == 0):
