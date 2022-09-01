669024295b15b303d703e4811ab8688e6226aebf
Jonathan Okah <jonaben7@gmail.com>
2022-09-01 16:42:03 +0100

    python dict

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    return a_dictionary.pop(key, None)
+    new_dict = {keys: val for keys,
+            val in a_dictionary.items() if key != key}
+    return new_dict
