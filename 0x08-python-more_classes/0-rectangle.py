0706b94e455ea47552d75e366508470c60d3f8f6
Jonathan <jonaben7@gmail.com>
2022-09-19 22:01:59 +0100

    creating an empty class

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,2 +1,13 @@
+#!/usr/bin/python3
+"""
+Rectangle:
+    creating an empty class
+    Returns nothing
+"""
 class Rectangle:
     pass
+
+    """
+    Creating an empty class that returns nothing
+
+    """
