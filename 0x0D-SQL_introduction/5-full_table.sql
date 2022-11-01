d3da1a9b36b1026a4ed357b286faead2d1518597
Jonaben1 <jonaben7@gmail.com>
2022-11-02 00:59:33 +0100

    sql scripts

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,6 +1,3 @@
 -- a script that prints the full description of a table
--- 'first-table' from the database 'hbtn_c0_0
-SHOW TABLE
-    first_table
-FROM
-    hbtn_0c_0;
+-- first-table from the database hbtn_c0_0
+SHOW TABLE first_table FROM hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,2 @@
+-- a script tha list all rows of the table
+SELECT * FROM first_table;
