222507d40e50ea7b11d071225f5ab10e26f06add
Jonathan <jonaben7@gmail.com>
2022-11-02 09:02:33 +0100

    Sql commmands getting complex

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- a script that displays the number of recirda
+-- in the table
+SELECT COUNT(id) FROM first_table;
---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,3 @@
+-- a script that creates a table in the database
+-- in NySQL server and add multiple rows
+CREATE TABLE IF NOT EXISTS second_table (id int, name VARCHAR(256), score int) VALUES (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
