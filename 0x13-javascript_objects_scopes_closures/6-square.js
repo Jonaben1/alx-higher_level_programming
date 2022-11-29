26bcd577e91ca0feec1a3fa5465cb12b674de891
Jonathan Okah <jonaben7@gmail.com>
2022-11-29 21:00:46 +0100

    JavaaScript is amazing

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const Base  = require('./5-square');
-module.exports  = class Square extends Base {
+const Base = require('./5-square');
+module.exports = class Square extends Base {
   charPrint (c) {
     const char = c === undefined ? 'X' : c;
     for (let i = 0; i < this.height; i++) {
       console.log(char.repeat(this.width));
     }
   }
-}
+};
