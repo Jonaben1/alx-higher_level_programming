0bf534bbe0555acaf1dace69b64ee75823312cbe
Jonathan Okah <jonaben7@gmail.com>
2022-12-15 15:22:56 +0100

    Python Network

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,21 +1,19 @@
-#!/bin/bash/python3
-"""
-Define a peak-finding algorithm
-"""
+#!/usr/bin/python3
+"""Defines a peak-finding algorithm."""
 
 
 def find_peak(list_of_integers):
-    """
-    Return a peak in a list of unsorted integer
-    """
+    """Return a peak in a list of unsorted integers."""
     if list_of_integers == []:
         return None
-    leng = len(list_of_integers)
-    if leng == 1:
+
+    size = len(list_of_integers)
+    if size == 1:
         return list_of_integers[0]
-    elif leng == 2:
+    elif size == 2:
         return max(list_of_integers)
-    mid = int(leng / 2)
+
+    mid = int(size / 2)
     peak = list_of_integers[mid]
     if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
         return peak
