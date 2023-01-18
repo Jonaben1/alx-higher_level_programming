94e628b298bf8f077d5b05b44ae410bff95eedb2
Jonathan Okah <jonaben7@gmail.com>
2023-01-18 12:17:55 +0100

    initialcommit

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
-const request = require(request);
-request.get(process.argv[2]).on(response, function (response) {
-  console.log(`code: ${response.statusCode}`);
+// JS Script
+require(request).get(process.argv[2], function (err, r) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(code:  + r.statusCode);
+  }
 });
